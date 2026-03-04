d840ddadc9eecc81f9930bfe02722591c518f911
Tom Ranner <T.Ranner@leeds.ac.uk>
2026-03-04 13:00:44 +0000

    update to more recent version of simple worm

--- forward_model.py
+++ forward_model.py
@@ -1,17 +1,30 @@
+from numpy.linalg import norm
+from dolfinx import fem
+from matplotlib import pyplot as plt
 import numpy as np
-from fenics import *
+import ufl
 
 from simple_worm.controls import (
     ControlsFenics,
     ControlsNumpy,
+    ControlSequenceFenics,
 )
 from simple_worm.worm import Worm
+from simple_worm.util import f2n, v2f
 
 # Parameters
-N = 10  # Number of body points - recommend ~100
-T = 1.0  # Final time - recommend several undulations
-dt = 0.1  # Time step - recommend ~1.0e-2 or lower
+N = 100  # Number of body points - recommend ~100
+T = 1.0e-2  # Final time - recommend several undulations
+dt = 1.0e-3  # Time step - recommend ~1.0e-2 or lower
 n_timesteps = int(T / dt)
+
+
+def plot_curve(x, filename="_tmp.png"):
+    plt.figure(1)
+    plt.plot(x[0], x[2])
+
+    plt.axis("equal")
+    plt.savefig(filename)
 
 
 def example1():
@@ -23,13 +36,6 @@ def example1():
     worm = Worm(N, dt)
     worm.initialise()
 
-    u = SpatialCoordinate(worm.V)[0]
-    control = Function(worm.V)
-
-    # holder for other control directions
-    zero = Function(worm.V)
-    zero.vector = 0
-
     # wave parameters
     A = 10.0
     lam = 1.5
@@ -37,28 +43,49 @@ def example1():
 
     # specific forcing function
     def alpha_forcing(t):
-        return A * sin(2.0 * pi * lam * u - 2 * pi * omega * t)
+        def alpha_forcing_t(u_):
+            u = u_[0]  # convert 3d coordinate to 1d
+            return A * np.sin(2.0 * np.pi * lam * u - 2 * np.pi * omega * t)
+
+        return alpha_forcing_t
+
+    def zero_forcing(u):
+        return 0.0 * u[0]
 
     t = 0.0
     while t < T:
         t += dt
 
-        # update control
-        project(alpha_forcing(t), function=control)
-
         # solve
-        ret = worm.update_solution(ControlsFenics(alpha=control, beta=zero, gamma=zero))
+        ret = worm.update_solution(
+            ControlsFenics(
+                alpha=v2f(alpha_forcing(t), fs=worm.V),
+                beta=v2f(zero_forcing, fs=worm.V),
+                gamma=v2f(zero_forcing, fs=worm.Q),
+            )
+        )
 
         # output variables as 'fenics functions
-        # x = ret.x
-        # curvature = ret.alpha
+        x = ret.x
+        vector_curvature = ret.kappa_expr
+
+        # other variables computed
+        tangent = ret.e0
+        normal = ret.e1
+
+        # scalar curvature
+        alpha = ufl.dot(vector_curvature, normal)
+
+        # using the variables to compute interesting quantities
+        curvature_form = fem.form(0.5 * alpha**2 * ufl.dx)
+        total_curvature = fem.assemble_scalar(curvature_form)
+        print(t, total_curvature)
 
         ret_np = ret.to_numpy()
         x_np = ret_np.x
         curvature_np = ret_np.alpha
 
-        print(f"{x_np=}")
-        print(f"{curvature_np=}")
+        plot_curve(x_np)
 
 
 def example2():
@@ -66,12 +93,13 @@ def example2():
     This example shows how to call the simulator with a fenics function
     for forcing.
     """
+    N = 120
     # holders for 'worm', u and control
     worm = Worm(N, dt)
     worm.initialise()
 
     # controls holder but we will only use 7 different values
-    N_controls = 7
+    N_controls = 6
     control = np.empty(N)
 
     # holder for other control directions
@@ -87,7 +115,7 @@ def example2():
     def alpha_forcing(t, j):
         # j is point in numpy array
         # j_control is the corresponding control point
-        j_control = j * N // N_controls
+        j_control = (j * N_controls) // N
         # u_control is center point of control region
         u_control = (j_control + 0.5) / N_controls
         return A * np.sin(2.0 * np.pi * lam * u_control - 2 * np.pi * omega * t)
@@ -113,8 +141,9 @@ def example2():
 
         print(f"{x_np=}")
         print(f"{curvature_np=}")
+        plot_curve(x_np)
 
 
 if __name__ == "__main__":
     example1()
-    example2()
+    # example2()
