e50dca27beabf9480e4df3472b048cb5f8c02090
Harrison Loftus <harrisonjloftus@gmail.com>
2026-07-06 15:07:46 +0100

    Implemented model into simple worm: works for N = 12, 24.
    Next steps: add nondimensionalised e and eta_tilde, test spatial and temporal distancing

---
+++ scripts/Johnson-Ranner/johnson-ranner.py
@@ -0,0 +1,160 @@
+from numpy.linalg import norm
+from dolfinx import fem
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.integrate import solve_ivp
+import ufl
+
+from simple_worm.controls import (
+    ControlsFenics,
+    ControlsNumpy,
+    ControlSequenceFenics,
+)
+from simple_worm.material_parameters import MaterialParameters, MaterialParametersFenics
+from simple_worm.worm import Worm
+from simple_worm.util import f2n, v2f
+
+from hilbert_calc import Hilbert_Transform
+from simple_worm_viewer_pyqtgraph import view_worm_pyqtgraph
+from curvature_viewer import view_curvature_pyqtgraph
+
+from ODEs import *
+
+
+def example4():
+    """
+    This example shows how to call the simulator with a fenics function
+    for forcing with a different set of material parameters
+    """
+    # holders for 'worm', u and control
+    # Parameters
+
+    T = 10.0  # Final time - recommend several undulations
+    global dt
+    dt = 1.0e-2  # Time step - recommend ~1.0e-2 or lower
+    global t_eval
+    t_eval = np.arange(0,T,dt)
+
+    n_timesteps = int(T / dt)
+    s = np.linspace(0.0,1.0,N)
+
+    ODE_state = np.zeros(4*N)
+    ODE_state[2*N:3*N] = 0.5
+    ODE_state[3*N:4*N] = -0.5
+
+    kappa_init = np.zeros(N)
+
+    
+    def pref_curvature_at_u(u, betas):
+        beta_idx = np.argmin(np.abs(u - s))
+        return betas[beta_idx]
+
+    
+    # holders for 'worm', u and control
+    worm = Worm(N, dt)
+
+    # set material parameters
+    MP = MaterialParameters(
+        K=10.0,  # ratio of drag coefficients
+        K_rot=1.0,  # rotational drag coefficient
+        A=1.0,  # bending rigidity
+        B=0.1,  # bending viscosity
+        C=1.0,  # twisting rigidity
+        D=0.1,  # twisting viscosity
+    )
+    worm.initialise(MP)
+
+    
+    control = np.empty(N)
+    
+    # holder for other control directions
+    zeroN = np.zeros(N)
+    zeroNm = np.zeros(N - 1)
+    
+   
+    # specific forcing function
+    def alpha_forcing(t, j):
+        
+        # j is point in numpy array
+        # j_control is the corresponding control point
+        j_control = (j * N_controls) // N
+        
+        # u_control is center point of control region
+        u_control = (j_control + 0.5) / N_controls
+        a = pref_curvature_at_u(u_control, betas)
+        
+        return a
+
+    t = 0.0
+    kappas = []
+    worm_positions = []
+
+    while t < T:
+
+        sols = solve_ivp(ODEs, (t, t+dt), ODE_state, method="RK45",
+                        max_step=dt,args=(kappa_init,))
+
+        ODE_state = sols.y[:, -1]
+
+        A_V = ODE_state[0:N]
+        A_D = ODE_state[N:2*N]
+
+
+        t += dt
+
+        betas = sigma(A_V) - sigma(A_D)        
+        
+        # update control
+        control[:] = [alpha_forcing(t, j) for j in range(N)]
+
+        # solve
+        Cntrl = ControlsNumpy(alpha=control, beta=zeroN, gamma=zeroNm)
+        ret = worm.update_solution(Cntrl.to_fenics(worm))
+
+        # output variables as 'fenics functions
+        vector_curvature = ret.kappa_expr
+
+        # other variables computed
+        tangent = ret.e0
+        normal = ret.e1
+
+        # scalar curvature
+        alpha = ufl.dot(vector_curvature, normal)
+
+        # using the variables to compute interesting quantities
+        curvature_form = fem.form(0.5 * alpha**2 * ufl.dx)
+        total_curvature = fem.assemble_scalar(curvature_form)
+        #print(t, total_curvature)
+
+        ret_np = ret.to_numpy()
+        x_np = ret_np.x
+        x_np_frame = x_np.T
+        
+        curvature_np = ret_np.alpha
+        
+        
+        worm_positions.append(x_np_frame.copy())
+        kappas.append(curvature_np.copy())
+        kappa_init = curvature_np.copy()
+
+        #print(np.max(betas), np.max(kappa_init))
+        
+    kappas = np.array(kappas)
+    worm_positions = np.array(worm_positions)
+    return worm_positions, kappas.T
+
+
+if __name__ == "__main__":
+
+
+    worm_positions, curvatures = example4()
+
+    maxcurv = np.max(curvatures)
+    wave, freq = Hilbert_Transform(curvatures, N, N_controls, t_eval)
+
+    print("Wavelength: ", np.round(wave, 2))
+    print("Frequency Hz: ", np.round(freq, 2))
+    print("Max curvature: ", np.round(maxcurv, 2))
+   
+    view_worm_pyqtgraph(worm_positions, dt)
+    #view_curvature_pyqtgraph(curvatures, dt)
